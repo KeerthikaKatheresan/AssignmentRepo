3325319fbb3976c837f7a8bd2aee1b21d43e1209
Keerthika Katheresan <keerthikakatheresan@gmail.com>
2025-11-23 23:56:51 +0530

    Add files via upload
    Nov 20

---
+++ CurrentAccount.java
@@ -0,0 +1,29 @@
+package GroupId1.Demo;
+
+public class CurrentAccount extends Account {
+	
+	    private double overdraftLimit;
+
+	    public CurrentAccount(long accNo, String holderName, double balance, double overdraftLimit, String bankName, String bankBranch) {
+	        super(accNo, holderName, balance, bankName, bankBranch);
+	        this.overdraftLimit = overdraftLimit;
+	    }
+
+	    @Override
+	    public void withdraw(double amount) {
+	        if(amount <= getBalance() + overdraftLimit) {
+	            setBalance(getBalance() - amount);
+	            System.out.println("Current Account: Withdrawal successful. New Balance: " + getBalance());
+	        } else {
+	            System.out.println("Current Account: Overdraft Limit Exceeded.");
+	        }
+	    }
+
+	    @Override
+	    public void deposit(double amount) {
+	        setBalance(getBalance() + amount);
+	        System.out.println("Current Account: Deposit successful. New Balance: " + getBalance());
+	    }
+	}
+
+
